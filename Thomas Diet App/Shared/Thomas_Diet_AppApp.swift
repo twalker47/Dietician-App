4b477515de4a59aa1e1fc68af2913ed21cb8cd9b
laurenwinslett <112721347+laurenwinslett@users.noreply.github.com>
2022-09-15 13:04:01 -0500

    duplicated project for app

---
+++ Thomas Diet App/Shared/Thomas_Diet_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thomas_Diet_AppApp.swift
+//  Shared
+//
+//  Created by Thomas Walker on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Thomas_Diet_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
