895c710e97182d37b23de37829e0481fb1bedb4f
gregyoung232 <gregyoung232@gmail.com>
2022-10-25 17:21:56 -0500

    added mifflin output
    Need to work out how to choose which results output to results view

--- CalCount/CalCount/CalCountApp.swift
+++ CalCount/CalCount/CalCountApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct CalCountApp: App {
     var body: some Scene {
         WindowGroup {
-            SplashScreenView()
+            //SplashScreenView()
+            //ContentView()
+            MiffinStJeor()
         }
     }
 }
--- CalCount/CalCount/MiffinStJeor.swift
+++ CalCount/CalCount/MiffinStJeor.swift
@@ -6,8 +6,21 @@
 //
 
 import SwiftUI
-
+public var mifflin_statement1 = ""
+public var mifflin_statement2 = ""
 struct MiffinStJeor: View {
+    @State var weight = 0
+    @State var height = 0
+    @State var age = 0
+    @State var showingDetail = false
+    
+    
+    let formatter: NumberFormatter = {
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .decimal
+            return formatter
+        }()
+    
     var body: some View {
         
         
@@ -19,12 +32,36 @@ struct MiffinStJeor: View {
                 .font(.system(size: 30))
             
             VStack{
-                Button("Stuff for Miffin Equation ") {
-                
-                }
-               
+                Text("Mifflin-St. Jeor")
+                HStack(alignment: .center){
+                    Text("Weight: ")
+                    TextField("Enter patient's weight", value: $weight, formatter: formatter).textFieldStyle(RoundedBorderTextFieldStyle()).keyboardType(.decimalPad)
+                }.padding()
+                HStack(alignment: .center){
+                    Text("Height: ")
+                    TextField("Enter patient's height", value: $height, formatter: formatter).textFieldStyle(RoundedBorderTextFieldStyle()).keyboardType(.decimalPad)
+                }.padding()
+                HStack(alignment: .center){
+                    Text("age: ")
+                    TextField("Enter patient's age", value: $age, formatter: formatter).textFieldStyle(RoundedBorderTextFieldStyle()).keyboardType(.decimalPad)
+                }.padding()
+                Button(action: {
+                    self.showingDetail.toggle()
+                    print(weight)
+                    print(height)
+                    print(age)
+                    let mifflinResultM = mifflin(sex: true, weight: Float(weight), height: Float(height), age: Float(age))
+                    mifflin_statement1 = "Calories for male patient: \(mifflinResultM) kcal"
+                    print(mifflin_statement1)
+                    let mifflinResultF = mifflin(sex: false, weight: Float(weight), height: Float(height), age: Float(age))
+                    mifflin_statement2 = "Calories for female patient: \(mifflinResultF) kcal"
+                    print(mifflin_statement2)
+                }) {
+                    Text("Calculate")
+                }.sheet(isPresented: $showingDetail) {
+                    Results()
+                }.padding()
             }
-            .buttonStyle(.bordered)
         }
     }
 }
@@ -35,4 +72,6 @@ struct MiffinStJeor_Previews: PreviewProvider {
         MiffinStJeor()
     }
 }
+
+
 
