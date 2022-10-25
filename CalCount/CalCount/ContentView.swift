9b823c3e4863fd5d70560d1a9521d03eb70a867c
twalker47 <91645389+twalker47@users.noreply.github.com>
2022-10-24 21:03:07 -0500

    worked on UI

--- CalCount/CalCount/ContentView.swift
+++ CalCount/CalCount/ContentView.swift
@@ -20,31 +20,6 @@ struct ContentView: View {
             
             VStack{
                 Button("Guided Mode") {
-                    var view = UILabel()
-
-                    view.frame = CGRect(x: 0, y: 0, width: 306, height: 145)
-
-                    view.backgroundColor = .white
-
-
-                    view.layer.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0).cgColor
-
-
-                    var parent = self.view!
-
-                    parent.addSubview(view)
-
-                    view.translatesAutoresizingMaskIntoConstraints = false
-
-                    view.widthAnchor.constraint(equalToConstant: 306).isActive = true
-
-                    view.heightAnchor.constraint(equalToConstant: 145).isActive = true
-
-                    view.leadingAnchor.constraint(equalTo: parent.leadingAnchor, constant: 20).isActive = true
-
-                    view.topAnchor.constraint(equalTo: parent.topAnchor, constant: 177).isActive = true
-
-
             }
                 Button("Manual Mode"){
                     
---
+++ CalCount/CalCount/SwiftUIView.swift
@@ -0,0 +1,27 @@
+//
+//  SwiftUIView.swift
+//  CalCount
+//
+//  Created by Thomas Walker on 10/24/22.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    
+    var body: some View {
+        Text("Hello, World!")
+            .font(.title)
+            .fontWeight(.medium)
+            .foregroundColor(Color("SageGreen"))
+            
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+
+    
+}
