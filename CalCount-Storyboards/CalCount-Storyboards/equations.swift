273234e7baf4b6cd2420cf839210c5d3871d88e8
gregyoung232 <gregyoung232@gmail.com>
2022-11-14 17:45:33 -0600

    added HB, Livingston, and PennState2003 equation

--- CalCount-Storyboards/CalCount-Storyboards/equations.swift
+++ CalCount-Storyboards/CalCount-Storyboards/equations.swift
@@ -36,14 +36,39 @@ func mifflin(sex: Bool, weight: Double, height: Double, age: Double) -> Double {
     }
 }
 
-/*
-func pennStateH(sex: Bool, weight: Float, height: Float, age: Float, tMax: Float) ->Float{
-    let result = 0.94 * mifflin(sex: sex, weight: weight, height: height, age:age) + tMax * 186 - 6597
+func harris(sex: Bool, weight: Double, height: Double, age: Double) -> Double {
+    if (sex==true) {
+        let result = 655.1 + 9.53 * weight + 1.85 * height - 4.676 * age
+        return result
+    }
+    else {
+        let result = 66.5 + 13.75 * weight + 5.003 * height - 6.75 * age
+        return result
+    }
+}
+
+func livingston(sex: Bool, weight: Double, age: Double) -> Double {
+    if (sex==true) {
+        let result = 293 * pow(weight, 0.4330) - 5.92 * age
+        return result
+    }
+    else {
+        let result = 248 * pow(weight, 0.4356) - 5.09 * age
+        return result
+    }
+}
+
+func pennState3(sex: Bool, weight: Double, height: Double, age: Double, v: Double, tMax: Double) -> Double {
+    let result = 0.96 * mifflin(sex: sex, weight: weight, height: height, age: age) + v * 31 + tMax * 167 - 6212
     return result
 }
 
-func pennState2010(sex: Bool, weight: Float, height: Float, age: Float, v: Float, tMax: Float) ->Float{
+func pennState2010(sex: Bool, weight: Double, height: Double, age: Double, v: Double, tMax: Double) ->Double{
     let result = 0.71 * mifflin(sex: sex, weight: weight, height: height, age:age) + v * 64 + tMax * 85 - 3085
     return result
 }
-*/
+
+func pennStateH(sex: Bool, weight: Double, height: Double, age: Double, tMax: Double) ->Double{
+    let result = 0.94 * mifflin(sex: sex, weight: weight, height: height, age:age) + tMax * 186 - 6597
+    return result
+}
