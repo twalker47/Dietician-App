6cf6fe920381cb611ea108880703bcac42be0cb9
gregyoung232 <gregyoung232@gmail.com>
2022-12-11 19:23:21 -0600

    adjusted keyboard view and changed app name

--- CalCount-Storyboards/CalCount-Storyboards/PennStateHControllerViewController.swift
+++ CalCount-Storyboards/CalCount-Storyboards/PennStateHControllerViewController.swift
@@ -95,10 +95,33 @@ class PennStateHControllerViewController: UIViewController {
         resultsController.myResults = pennStateHResult
     }
     
+    @objc func keyboardWillShow(notification: NSNotification) {
+            
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+           // if keyboard size is not available for some reason, dont do anything
+           return
+        }
+      
+      // move the root view up by the distance of keyboard height
+      self.view.frame.origin.y = 0 - keyboardSize.height
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
-        
+      
+        // call the 'keyboardWillShow' function when the view controller receive the notification that a keyboard is going to be shown
+        NotificationCenter.default.addObserver(self, selector: #selector(PennState2003Controller.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+      
+          // call the 'keyboardWillHide' function when the view controlelr receive notification that keyboard is going to be hidden
+        NotificationCenter.default.addObserver(self, selector: #selector(PennState2003Controller.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+      // move back the root view origin to zero
+      self.view.frame.origin.y = 0
     }
     
 
